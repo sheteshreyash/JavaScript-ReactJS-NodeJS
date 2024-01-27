a6102c0696de18258c92d298b4ac9a210ff56eb0
sheteshreyash <shreyashshete21@gmail.com>
2024-01-27 20:04:50 +0530

    added variables in js

--- JS Codes/01_Variables.js
+++ JS Codes/01_Variables.js
@@ -1,2 +1,14 @@
 // Variables in javascript
 
+let a = 67  // We will use 'let' function to declare a which contains 67
+console.log(a)
+a = "Shreyash"  // type of a will change from integer to string
+console.log(a)
+
+// javascript allows to change the variables types at runtime
+// and that's why it is called dynamic type programming language
+
+// let 8shrey = 9  // This will throw error
+
+// JavaScript is a Case Sensitive Programming Language
+
