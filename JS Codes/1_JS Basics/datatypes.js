9b767223454eea8356f34d2a70695c455aaa44d9
sheteshreyash <shreyashshete21@gmail.com>
2024-02-08 15:10:56 +0530

    completed chapter 1

---
+++ JS Codes/1_JS Basics/datatypes.js
@@ -0,0 +1,22 @@
+// datatypes and objects in js
+
+// 7 primitive datatypes NN SS BB U - Null Number String Symbol Boolean BigInt Undefined
+// objects are key value pairs
+
+// primitive datatypes :-
+let a = null
+let b = 686
+let c = 'shrey'
+let d = BigInt("5673356")
+let e = true
+let f = undefined
+let g = Symbol("&&6783")
+console.log(a, b, c, d, e, f, g)
+
+
+// object :- (non primitive datatypes)
+const item = {
+    name: 'Led Bulb',
+    price: 150
+}
+console.log(item["name"])
---
+++ JS Codes/1_JS Basics/letvarconst.js
@@ -0,0 +1,29 @@
+// var concept
+var a = 56
+var a = "hi there"
+console.log(a, "\n")
+var b = "shrey"
+
+
+// let concept
+let c = 78
+c = "how are you"
+{
+    let c = "shete"
+    console.log(c)
+}
+console.log(c)
+
+
+// const concept
+const auth = "Shreyaaaa"
+// const ors // const declarations must be initialized
+// let auth = 6788   // throw error because constant value cannot be changed
+// auth = 678   // assigned to constant value
+console.log(auth)
+{
+    const auth = "hwllloooo"
+    console.log(auth)
+}
+
+// must not be use var, use const maximum times and in some cases, use let
