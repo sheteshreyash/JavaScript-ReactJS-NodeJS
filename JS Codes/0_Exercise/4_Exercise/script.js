bed49cf2513ad9d380079bdbafd3eaeeb5e8dd74
sheteshreyash <shreyashshete21@gmail.com>
2024-02-20 19:28:06 +0530

    completed exercise 4

--- JS Codes/0_Exercise/4_Exercise/script.js
+++ JS Codes/0_Exercise/4_Exercise/script.js
@@ -5,15 +5,37 @@
 
 
 
-function updateClock() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    const seconds = String(now.getSeconds()).padStart(2, "0");
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById("time").textContent = timeString;
+function getDateTime() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    if (month.toString().length == 1) {
+        month = "0" + month;
+    }
+    if (day.toString().length == 1) {
+        day = "0" + day;
+    }
+    if (hour.toString().length == 1) {
+        hour = "0" + hour;
+    }
+    if (minute.toString().length == 1) {
+        minute = "0" + minute;
+    }
+    if (second.toString().length == 1) {
+        second = "0" + second;
+    }
+    var dateTime =
+        year + "/" + month + "/" + day + " " + hour + ":" + minute + ":" + second;
+    return dateTime;
 }
 
-setInterval(updateClock, 1000);
-updateClock();
+// example usage: realtime clock
+setInterval(function () {
+    currentTime = getDateTime();
+    document.getElementById("time").innerHTML = currentTime;
+}, 1000);
 
