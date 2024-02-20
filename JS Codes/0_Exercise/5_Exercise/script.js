bf25a3186a0adba5dbb46f005e65fecaff40741c
sheteshreyash <shreyashshete21@gmail.com>
2024-02-21 01:01:34 +0530

    completed exercise 5

--- JS Codes/0_Exercise/5_Exercise/script.js
+++ JS Codes/0_Exercise/5_Exercise/script.js
@@ -14,27 +14,177 @@
 
 
 
-const terminal = document.getElementById("terminal");
+// let a = [
+//   "Initializing Hack tool...",
+//   "Connecting to Facebook...",
+//   "Connecting to server 1...",
+//   "Connection failed. Retrying...",
+//   "Connecting to server 2",
+//   "Connected Successfully...",
+//   "Username iamshreyash...",
+//   "Trying Brute Force...",
+//   "200K passwords tried...",
+//   "Match not found",
+//   "Another 200K passwords tried...",
+//   "Match not found...",
+//   "Another 200K passwords tried...",
+//   "Match not found...",
+//   "Another 200K passwords tried...",
+//   "Match found...",
+//   "Accessing Account...",
+//   "Hack Successful..."
+// ]
 
-function typeMessage(message, delay) {
-    return new Promise((resolve) => {
-    setTimeout(() => {
-      terminal.innerHTML += message + "<br>";
-      terminal.scrollTop = terminal.scrollHeight;
-      resolve();
-    }, delay);
-  });
+// const sleep = async (seconds) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => { resolve(true) }, seconds * 1000)
+//   })
+// }
+
+// const showHack = async (message) => {
+//   await sleep(2)
+//   // console.log(message)
+//   text.innerHTML = text.innerHTML + message + "<br>"
+// }
+
+// (async () => {
+//   for (let i = 0; i < a.length; i++) {
+//     await showHack(a[i])
+//   }
+// })()
+
+
+
+
+
+
+const genCode = () => {
+  let code = '';
+  while (code.length < 1000) {
+    code = code.concat(Math.floor(Math.random() * 2));
+  }
+  return code;
 }
 
-async function hack() {
-  await typeMessage("Initializing Network", 1000);
-  await typeMessage("Connecting to service...", 1500);
-  await typeMessage("Retrieving username...", 2000);
-  await typeMessage("Username found...", 1000);
-  await typeMessage("Trying a combination of 4.5 Trillion passwords...", 2000);
-  await typeMessage("Password found...", 1000);
-  await typeMessage("Connecting to Facebook...", 1500);
-  // You can add more steps here
+let check = Array.from(document.getElementsByClassName("check"))
+
+function checkall() {
+  if (check[2].checked) {
+    check[0].checked = true
+    check[1].checked = true
+  }
+}
+function hack() {
+  let side = Array.from(document.getElementsByClassName("side1"));
+  let clear1 = setInterval(() => {
+    side[0].innerHTML = genCode();
+  }, 300)
+  let clear2 = setInterval(() => {
+    side[1].innerHTML = genCode();
+  }, 250)
+  let text = Array.from(document.getElementsByClassName("text"))
+  let target = document.getElementById("target").value;
+
+  // Async Await
+
+  const hackapi = async function () {
+    // clear screen
+    {
+      for (let i = 1; i < text.length; i++) {
+        text[i].innerHTML = " "
+      }
+    }
+    // screen cleared
+
+    let step1 = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[1].innerHTML = "Hijacking user..."
+        resolve(1)
+      }, 1000)
+    })
+    let step2 = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[2].innerHTML = "user located"
+        resolve(1)
+      }, 1000)
+    })
+    let step1a = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[2].innerHTML = `username:${target} ; `
+        resolve(1)
+      }, 1000)
+    })
+    let step1b = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[2].innerHTML += ` password:XXXXXXX ;`
+        resolve(1)
+      }, 500)
+    })
+    let step1c = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[2].innerHTML += ` number:+91XXXXXXXXXX`
+        resolve(1)
+      }, 800)
+    })
+    let step3 = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[3].innerHTML = "getting device info..."
+        resolve(1)
+      }, 1000)
+    })
+    if (check[0].checked) {
+      let step5 = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          text[4].innerHTML = "facebook hacked"
+          resolve(1)
+        }, 1000)
+      })
+    }
+    if (check[1].checked) {
+      let step5 = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          text[4].innerHTML = "Instagram hacked"
+          resolve(1)
+        }, 1000)
+      })
+    }
+    let step5 = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[4].innerHTML = "resetting passwords..."
+        resolve(1)
+      }, 1000)
+    })
+    let step6 = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[2].innerHTML = "access granted to phone"
+        resolve(1)
+      }, 1000)
+    })
+    let step7 = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[2].innerHTML = "logs generated"
+        resolve(1)
+      }, 1000)
+    })
+    let step8 = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[5].innerHTML = "hacking status: pending..."
+        resolve(1)
+      }, 1000)
+    })
+    let step9 = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        text[5].innerHTML = "hacking status: hacked! succesfully -_+"
+        resolve(1)
+        clearInterval(clear1)
+        clearInterval(clear2)
+        side.forEach((x) => {
+          x.innerHTML = "Hacker Man"
+        })
+      }, 3000)
+    })
+  }
+  hackapi()
 }
 
-hack();
+
